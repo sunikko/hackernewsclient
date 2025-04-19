d3e68666c0489e1a9af2ba3b779558b5432c4329
Soy <soyeon.won@oxbridgeuk.com>
2025-04-19 17:49:02 +0100

    Applied Tailwind CSS classes to news feed and detail view for cleaner layout

--- app.js
+++ app.js
@@ -39,16 +39,49 @@ function newsDetail() {
 
   console.log(newsContent);
 
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div>
+        <strong>${newsContent.user}</strong> ${newsContent.time_ago}  
+      </div>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
+  `;
+
   function makeComment(comments, called = 0) {
     const commentString = [];
 
     for (let i = 0; i < comments.length; i++) {
       commentString.push(`
-        <div style="padding-left: ${called * 40}px;">
-          <div>
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
             <strong>${comments[i].user}</strong> ${comments[i].time_ago}
           </div>
-          <p>${comments[i].content}</p>
+          <p class="text-gray-700">${comments[i].content}</p>
         </div>      
       `);
 
@@ -60,25 +93,10 @@ function newsDetail() {
     return commentString.join("");
   }
 
-  const commentString = makeComment(newsContent.comments);
-
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
-    <div>
-      <div>
-        <strong>${newsContent.user}</strong> ${newsContent.time_ago}  
-      </div>
-      <p>${newsContent.content}</p>
-    </div>
-    <div>
-      <h2>Comments</h2>
-      ${commentString} 
-    </div>
-
-    <div>
-      <a href="#/page/${store.currentPage}">back</a>
-    </div>
-  `;
+  container.innerHTML = template.replace(
+    "{{__comments__}}",
+    makeComment(newsContent.comments)
+  );
 }
 
 ////////////////////////////
@@ -88,13 +106,27 @@ function newsFeeds() {
   const newsFeed = getData(HEWS_URL);
   const newsList = [];
   let template = `
-    <h1>Hacker News</h1>
-    <ul>
-      {{__news_feed__}}
-    </ul>
-    <div>
-      <a href="#/page/{{__prev_page__}}">Previous</a>
-      <a href="#/page/{{__next_page__}}">Next</a>
+     <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
     </div>
   `;
 
@@ -106,11 +138,27 @@ function newsFeeds() {
     if (!newsFeed[i]) continue;
 
     newsList.push(`
-    <li>
-      <a href="#/show/${newsFeed[i].id}">
-        <strong>${newsFeed[i].title}</strong> ${newsFeed[i].comments_count}
-      </a>
-    </li>
+    <div class="p-6 ${
+      newsFeed[i].read ? "bg-red-500" : "bg-white"
+    } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
+              newsFeed[i].comments_count
+            }</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>   
     `);
   }
   template = template.replace("{{__news_feed__}}", newsList.join(""));
